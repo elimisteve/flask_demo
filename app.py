7ff2b4386b0c6829a5d6122c2f53a5a1843137bf
Steve Phillips <elimisteve@gmail.com>
2017-07-07 23:57:18 +0000

    First commit. Added app.py and templates/hello.html

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello World!"
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 5000)
