fc06eaf599d2d0312ba473e649c4ab4be28c9f2b
Steve Phillips <steve@tryingtobeawesome.com>
2018-01-18 15:27:31 -0800

    Flask: set strict_slashes = False; made default <name> url parameter clearer

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return "Welcome to the homepage"
 
 
-@app.route('/hello/')
+@app.route('/hello')
 @app.route('/hello/<name>')
-def hello(name=None):
+def hello(name='visitor'):
     return render_template('hello.html', name=name)
 
 
